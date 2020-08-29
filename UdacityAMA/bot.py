c21b04f97729f42a54582c25752dfdf94e2903da
lambertsbennett <lambertsbennett@gmail.com>
2020-08-29 14:35:17 -0700

    add python bot

---
+++ UdacityAMA/bot.py
@@ -0,0 +1,41 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from botbuilder.core import ActivityHandler, TurnContext
+from botbuilder.schema import ChannelAccount
+from flask import Config
+
+from botbuilder.ai.qna import QnAMaker, QnAMakerEndpoint
+from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
+from botbuilder.schema import ChannelAccount
+
+
+
+class AMABot(ActivityHandler):
+    # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+
+    def __init__(self, config: Config):
+        self.qna_maker = QnAMaker(
+         QnAMakerEndpoint(
+         knowledge_base_id=config.QNA_KNOWLEDGEBASE_ID,
+         endpoint_key=config.QNA_ENDPOINT_KEY,
+         host=config.QNA_ENDPOINT_HOST,
+   )
+)
+
+    async def on_members_added_activity(
+        self,
+        members_added: ChannelAccount,
+        turn_context: TurnContext
+    ):
+        for member_added in members_added:
+            if member_added.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity("Welcome to the Udacity AMA bot! Ask me a question and hopefully I can help!")
+
+    async def on_message_activity(self, turn_context: TurnContext):
+    # The actual call to the QnA Maker service.
+        response = await self.qna_maker.get_answers(turn_context)
+        if response and len(response) > 0:
+            await turn_context.send_activity(MessageFactory.text(response[0].answer))
+        else:
+            await turn_context.send_activity("No QnA Maker answers were found.")
